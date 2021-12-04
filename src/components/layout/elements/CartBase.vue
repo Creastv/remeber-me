b1c990e7996157d24271989ca127688d85eb9e9a
creastv <creastv@gmail.com>
2021-12-04 21:45:04 +0100

    Finish setup

--- src/components/layout/elements/CartBase.vue
+++ src/components/layout/elements/CartBase.vue
@@ -23,12 +23,12 @@ export default {};
 </script>
 
 <style scoped>
-/* .cart {
+.cart {
   background-color: #fff;
   border: 1px solid rgb(216, 216, 216);
   border-radius: 5px;
   box-shadow: 0px 2px 5px 5px rgba(0, 0, 0, 0.1);
   padding: 10px;
   margin-bottom: 20px;
-} */
+}
 </style>
